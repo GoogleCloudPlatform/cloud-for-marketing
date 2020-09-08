2a95af124be6f2a717f92def5ca800fc1486d0dc
Mohab Fekry <mfekry@google.com>
2020-09-08 11:44:19 +0200

    Update README.md

--- marketing-analytics/predicting/future-customer-value-segments/run_locally.sh
+++ marketing-analytics/predicting/future-customer-value-segments/run_locally.sh
@@ -21,7 +21,7 @@ fi
 mkdir ./output
 
 python fcvs_pipeline_csv.py --runner=DirectRunner \
-  --input_csv ./input_cdnow.csv \
+  --input_csv ./samples/input_cdnow.csv \
   --output_folder ./output/ \
   --customer_id_column_position 1 \
   --transaction_date_column_position 2 \
